8ac72553e8026f09561bf4fad0f95b1a16d70320
Hussein Mohammed <Husseinsaqr43@gmail.com>
2020-04-12 11:06:17 +0200

    SharedPreferences Android Java

---
+++ AndroidAppsExamples/app/src/main/java/com/example/crudapp/DetailsUser.java
@@ -0,0 +1,50 @@
+package com.example.crudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DetailsUser extends AppCompatActivity {
+
+    TextView mtvDetails;
+    Button mbtnLogout;
+    SharedPreferences sharedPreferences;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_user);
+        initialization();
+        onClick();
+    }
+
+    private void initialization() {
+        mtvDetails = findViewById(R.id.txtvDetails);
+        mbtnLogout = findViewById(R.id.btnLogout);
+        sharedPreferences = getSharedPreferences("userLogin", Context.MODE_PRIVATE);
+        intent = new Intent(DetailsUser.this, MainActivity.class);
+
+    }
+
+    private void onClick() {
+        mtvDetails.setText(" Hello : " + sharedPreferences.getString("username",null) + "\n User Password Is : " + sharedPreferences.getString("password",null));
+        mbtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                Toast.makeText(DetailsUser.this, "User Logout", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ AndroidAppsExamples/app/src/main/java/com/example/crudapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.crudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText metUsername, metPassword;
+    TextInputLayout mtlUser, mtlPassword;
+    Button mbtnLogin;
+    SharedPreferences sharedPreferences;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialization();
+        onClick();
+    }
+
+//    Initialization Views Method
+    private void initialization() {
+        metUsername = findViewById(R.id.etUsername);
+        metPassword = findViewById(R.id.etPassword);
+        mbtnLogin = findViewById(R.id.btnLogin);
+        mtlUser = findViewById(R.id.tlUsername);
+        mtlPassword = findViewById(R.id.tlPass);
+        sharedPreferences = getSharedPreferences("userLogin", Context.MODE_PRIVATE);
+        intent = new Intent(MainActivity.this, DetailsUser.class);
+    }
+
+//    Button onClick Method
+    private void onClick() {
+        mbtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameDB = metUsername.getEditableText().toString().trim();
+                String passwordDB = metPassword.getEditableText().toString().trim();
+                if (usernameDB.equals("Hussein")) {
+                    mtlUser.setError(null);
+                    mtlUser.setErrorEnabled(false);
+                    mtlUser.requestFocus();
+                    if (passwordDB.equals("456")) {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", usernameDB);
+                        editor.putString("password", passwordDB);
+                        editor.apply();
+                        mtlPassword.setError(null);
+                        mtlPassword.setErrorEnabled(false);
+                        mtlPassword.requestFocus();
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this, "Logined", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mtlPassword.setError("Password Error!");
+                        mtlPassword.setErrorEnabled(true);
+                        mtlPassword.requestFocus();
+                    }
+                } else {
+                    mtlUser.setError("User Not Exits!");
+                    mtlUser.setErrorEnabled(true);
+                    mtlUser.requestFocus();
+                }
+            }
+
+        });
+    }
+}
